6d36fc2c35b9245a376761b130076c0f0eead735
chennlang <13206251194@163.com>
2023-06-07 14:57:41 +0800

    minor: move materials dir

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -26,6 +26,7 @@ const htmlPlugin = (vs: Record<string, any> = {}) => {
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+  console.log(env)
   return {
     plugins: [
       vue(),
