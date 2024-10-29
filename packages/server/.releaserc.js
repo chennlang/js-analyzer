0952bfd1ff33312f93d8b0bba5a6463734a96360
chennlang <13206251194@163.com>
2024-10-29 18:13:33 +0800

    build: ci 更新

--- packages/server/.releaserc.js
+++ packages/server/.releaserc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  repositoryUrl: 'https://github.com/chennlang/js-analyzer.git',
   branches: ["main"], // 指定在哪个分支下要执行发布操作
   plugins: [
     "@semantic-release/commit-analyzer", // 解析 commit 信息，默认就是 Angular 规范
