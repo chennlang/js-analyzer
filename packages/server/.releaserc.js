7b0dc84f76f2d8be4d54349295c03927b079446f
chennlang <13206251194@163.com>
2024-10-29 17:46:07 +0800

    build: ci构建更新测试

--- packages/server/.releaserc.js
+++ packages/server/.releaserc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  branches: ["release"], // 指定在哪个分支下要执行发布操作
+  branches: ["main"], // 指定在哪个分支下要执行发布操作
   plugins: [
     "@semantic-release/commit-analyzer", // 解析 commit 信息，默认就是 Angular 规范
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
       {
-        changelogFile: "CHANGELOG.md", // 把发布日志写入该文件
+        changelogFile: "../../CHANGELOG.md", // 把发布日志写入该文件
       },
     ],
     "@semantic-release/npm", // 发布到NPM
@@ -14,7 +14,7 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md", "package.json"], // 前面说到日志记录和版本好是新增修改的，需要 push 回 Git
+        assets: ["../../CHANGELOG.md", "../../package.json"], // 前面说到日志记录和版本好是新增修改的，需要 push 回 Git
       },
     ],
   ],
