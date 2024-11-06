320bf938bb17ba1f0738fd7c6ad997b1bb0daa00
chennlang <13206251194@163.com>
2024-11-06 10:36:40 +0800

    fix: 去除 core 自动发布，改为手动发布

--- packages/server/.releaserc.js
+++ packages/server/.releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  repositoryUrl: 'https://github.com/chennlang/js-analyzer.git',
+  repositoryUrl: "https://github.com/chennlang/js-analyzer.git",
   branches: ["main"], // 指定在哪个分支下要执行发布操作
   plugins: [
     "@semantic-release/commit-analyzer", // 解析 commit 信息，默认就是 Angular 规范
@@ -7,7 +7,7 @@ module.exports = {
     [
       "@semantic-release/changelog",
       {
-        changelogFile: "../../CHANGELOG.md", // 把发布日志写入该文件
+        changelogFile: "CHANGELOG.md", // 把发布日志写入该文件
       },
     ],
     "@semantic-release/npm", // 发布到NPM
@@ -15,7 +15,7 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        assets: ["../../CHANGELOG.md", "../../package.json"], // 前面说到日志记录和版本好是新增修改的，需要 push 回 Git
+        assets: ["CHANGELOG.md", "package.json"], // 前面说到日志记录和版本好是新增修改的，需要 push 回 Git
       },
     ],
   ],
